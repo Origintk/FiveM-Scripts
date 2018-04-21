b9a3d19ccbd78c75c8d9a3bc53b734bc4157ddae
Sun <30409932+Origintk@users.noreply.github.com>
2018-04-20 23:27:03 -0300

    try fix static hospital

--- vrpex/vrp_basic_menu/cfg/buttons/static.lua
+++ vrpex/vrp_basic_menu/cfg/buttons/static.lua
@@ -19,26 +19,48 @@
 --police weapons // comment out the weapons if you dont want to give weapons.
 police_weapons = {}
 police_weapons["Equip"] = {function(player,choice)
-    vRPclient.giveWeapons(player,{
-	  ["WEAPON_COMBATPISTOL"] = {ammo=200},
-	  ["WEAPON_PUMPSHOTGUN"] = {ammo=200},
-	  ["WEAPON_NIGHTSTICK"] = {ammo=200},
-	  ["WEAPON_STUNGUN"] = {ammo=200}
+    local user_id = vRP.getUserId(player)	
+     vRPclient.giveWeapons(player,{
+	  ["WEAPON_COMBATPISTOL"] = {ammo=500},
+	  ["WEAPON_PUMPSHOTGUN"] = {ammo=500},
+	  ["WEAPON_PISTOL"] = {ammo=500},
+	  ["WEAPON_CARBINERIFLE"] = {ammo=500},
+	  ["WEAPON_NIGHTSTICK"] = {ammo=1}, 
+	  ["WEAPON_STUNGUN"] = {ammo=500},
+	  ["WEAPON_SPECIALCARBINE"] = {ammo=500},
+	  ["WEAPON_BULLPUPRIFLE"] = {ammo=500},
+	  ["WEAPON_COMBATPDW"] = {ammo=500},
+	  ["WEAPON_ADVANCEDRIFLE"] = {ammo=500},
+	  ["WEAPON_FLASHLIGHT"] = {ammo=1},
+	  ["WEAPON_SMG"] = {ammo=500},
+	  ["WEAPON_MICROSMG"] = {ammo=500},
+	  ["WEAPON_STUNGUN"] = {ammo=500}  
 	}, true)
-	BMclient.setArmour(player,100,true)
+	BMclient.setArmour(player,1000,true)
+	vRPclient.notify(player,"Voce  pegou ~g~ seus equipamentos Policiais")
 end}
 
 --medkit storage
 emergency_medkit = {}
 emergency_medkit["Take"] = {function(player,choice)
-	local user_id = vRP.getUserId(player) 
+	local user_id = vRP.getUserId(player)
+        vRP.tryPayment(user_id,100,false)then		
 	vRP.giveInventoryItem(user_id,"medkit",25,true)
 	vRP.giveInventoryItem(user_id,"pills",25,true)
+     vRPclient.notify(player,"Voce foi ~g~ recebeu equipamentos medicos")
+    else 
+      vRPclient.notify(player," ~r~ Voce nao tem dinheiro suficiente para o sistema do SUS de cura rapida")
+    end
 end}
 
 --heal me
 emergency_heal = {}
 emergency_heal["Heal"] = {function(player,choice)
-	local user_id = vRP.getUserId(player) 
-	vRPclient.setHealth(player,1000)
+    local user_id = vRP.getUserId(player)	
+     if  vRP.tryPayment(user_id,100,false)then	
+    vRPclient.setHealth(player,1000)
+	 vRPclient.notify(player,"Voce foi ~g~ curado por 100")
+    else 
+      vRPclient.notify(player," ~r~ Voce nao tem dinheiro suficiente para o sistema do SUS de cura rapida")
+    end
 end}
